a61a5e5e9c8169d5bee0619115d0614893f4fef8
jaikarans <jaikrns@gmail.com>
2021-08-27 16:58:07 +0530

    seprate plasmoid needs sudo to install

---
+++ dependent-plasmoids.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+USER_ID=`id -u`
+echo $USER_ID
+
+# check if script is not running as root
+    if [  $USER_ID -ne 0 ]
+then 
+    echo 'Please run this file as rootgit '
+    echo 'sudo sh install-plasmoids.sh'
+    exit
+fi
+
+# installing cmake for Netspeed Widget
+pacman -S --noconfirm cmake extra-cmake-modules
+
+echo
+echo '==> Installing plasmoids...'
+echo 
+
+# if repo alredy exist then delete it before cloning 
+if [ -d "./plasma-applet-netspeed-widget" ] 
+then
+    rm -rf ./plasma-applet-netspeed-widget
+fi
+
+git clone https://github.com/dfaust/plasma-applet-netspeed-widget.git
+echo 
+echo '# Installing Netspeed Widget...'
+cd plasma-applet-netspeed-widget
+mkdir build
+cd build
+cmake -DCMAKE_INASTALL_PREFIX=/usr ..
+make
+sudo make install
+
+# removing cloned repo from dotfiles repo
+cd ../..
+rm -rf plasma-applet-netspeed-widget
+
