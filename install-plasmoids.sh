9566d1a4cdb08eeb207f397e929cd3649e1394cf
jaikarans <jaikrns@gmail.com>
2021-08-27 17:04:30 +0530

    update

--- install-plasmoids.sh
+++ install-plasmoids.sh
@@ -1,45 +1,6 @@
 #!/bin/sh
 
-USER_ID=`id -u`
-echo $USER_ID
-
-# check if script is not running as root
-    if [  $USER_ID -ne 0 ]
-then 
-    echo 'Please run this file as rootgit '
-    echo 'sudo sh install-plasmoids.sh'
-    exit
-fi
-
-# installing cmake for Netspeed Widget
-pacman -S --noconfirm cmake extra-cmake-modules
-
-echo
-echo '==> Installing plasmoids...'
-echo 
-
-# if repo alredy exist then delete it before cloning 
-if [ -d "./plasma-applet-netspeed-widget" ] 
-then
-    rm -rf ./plasma-applet-netspeed-widget
-fi
-
-git clone https://github.com/dfaust/plasma-applet-netspeed-widget.git
-echo 
-echo '# Installing Netspeed Widget...'
-cd plasma-applet-netspeed-widget
-mkdir build
-cd build
-cmake -DCMAKE_INASTALL_PREFIX=/usr ..
-make
-sudo make install
-
-# removing cloned repo from dotfiles repo
-cd ../..
-rm -rf plasma-applet-netspeed-widget
-
-# login as normal user
-sh su $SUDO_USER
+# ###### Application title ###### #
 
 # if repo alredy exist then delete it before cloning 
 if [ -d "./apptitle-plasmoid" ] 
@@ -47,19 +8,27 @@ then
     rm -rf ./apptitle-plasmoid
 fi
 
+# remove if already exists
+rm -rf ~/.local/share/plasma/plasmoids/org.kde.plasma.betterinlineclock
+
 echo 
 echo '==> Installing Application title...'
 echo 
 git clone https://gitlab.com/aleixq/apptitle-plasmoid.git
 cd apptitle-plasmoid/org.communia.apptitle
 plasmapkg2 -i .
-
 #cleaing repo
 cd ../..
 rm -rf apptitle-plasmoid
 
 
 # ###### Better inline clock ####### #
+
+# if repo alredy exist then delete it before cloning 
+if [ -d "./kde-plasmoid-betterinlineclock" ] 
+then
+    rm -rf ./kde-plasmoid-betterinlineclock
+fi
 
 # remove if already exists
 rm -rf ~/.local/share/plasma/plasmoids/org.kde.plasma.betterinlineclock
